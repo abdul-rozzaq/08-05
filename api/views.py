721df7eff1d39a7acd118e5811dc64142e1b5714
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-11-19 16:19:27 +0500

    last

--- api/views.py
+++ api/views.py
@@ -13,17 +13,13 @@ from .serializers import FoodSerializer
 class FoodAPIView(APIView):
     queryset = Food.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
-
     serializer_class = FoodSerializer
 
     def post(self, request, *args, **kwargs):
-
         serializer: FoodSerializer = self.serializer_class(data=request.data)
-
         serializer.is_valid(raise_exception=True)
-
         serializer.save()
-
+        
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def get(self, request, *args, **kwargs):
@@ -38,21 +34,17 @@ class FoodDetailAPIView(APIView):
     permission_classes = [IsAdminUser]
 
     def get(self, request, pk: int):
-
-        self.get_object()
-
         obj = self.get_object()
         serializer = self.serializer_class(instance=obj)
+        
         return Response(serializer.data)
 
     def put(self, request, pk: int):
         obj = self.get_object()
         serializer = self.serializer_class(instance=obj, data=request.data)
-
         serializer.is_valid(raise_exception=True)
-
         serializer.save()
-
+        
         return Response(serializer.data)
 
     def delete(self, request, pk: int):
